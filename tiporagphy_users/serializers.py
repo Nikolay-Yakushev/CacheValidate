5147c33a714f89d84fc46fb219d584b509f3a3f7
driver220v <driver220v@gmail.com>
2021-03-29 09:38:52 +0300

    modefied docker-compose, remove redundant code

--- tiporagphy_users/serializers.py
+++ tiporagphy_users/serializers.py
@@ -100,20 +100,6 @@ class AuthenticateUserSerializer(serializers.Serializer):
 
 
 """Check if UserInGroup"""
-
-
-class CustomPKRelatedField(serializers.PrimaryKeyRelatedField):
-    """A PrimaryKeyRelatedField derivative that uses named field for the display value."""
-
-    def __init__(self, **kwargs):
-        self.display_field = kwargs.pop("display_field", "name")
-        super(CustomPKRelatedField, self).__init__(**kwargs)
-
-    def display_value(self, instance):
-        # Use a specific field rather than model stringification
-        return getattr(instance, self.display_field)
-
-
 class GroupsSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         grs = [
